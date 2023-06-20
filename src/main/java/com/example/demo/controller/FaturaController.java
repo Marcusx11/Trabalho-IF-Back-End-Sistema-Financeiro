62fc4eb9e9f287ec6c629a4bd700796fa2819a46
Mustardking <44874254+Marcusx11@users.noreply.github.com>
2023-06-20 14:18:51 -0300

    Criando repository e metodo de listar faturas

--- src/main/java/com/example/demo/controller/FaturaController.java
+++ src/main/java/com/example/demo/controller/FaturaController.java
@@ -1,9 +1,26 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.FaturaDTO;
+import com.example.demo.service.FaturaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/faturas")
 @RestController
 public class FaturaController {
+
+    @Autowired
+    private FaturaService service;
+
+    @GetMapping("/listar")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public List<FaturaDTO> listar() {
+        return service.listar();
+    }
 }
--- src/main/java/com/example/demo/service/FaturaService.java
+++ src/main/java/com/example/demo/service/FaturaService.java
@@ -1,7 +1,24 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.FaturaDTO;
+import com.example.demo.entity.Fatura;
+import com.example.demo.repository.FaturaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FaturaService {
+
+    @Autowired
+    private FaturaRepository repository;
+
+    public List<FaturaDTO> listar() {
+        List<Fatura> entidades = repository.findAll();
+
+        return entidades.stream().map(FaturaDTO::new).collect(Collectors.toList());
+    }
 }
